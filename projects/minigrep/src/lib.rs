0dd3ab9cbb3cc74ca25e92d2d47f032d43ea3ecd
uu64 <yusuke.t.0808@gmail.com>
2024-06-02 15:56:16 +0900

    fix: minigrep

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -3,32 +3,24 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
-// struct Config<'a> {
-//     query: &'a str,
-//     filename: &'a str,
-// }
+use clap::Parser;
 
+#[derive(Parser, Debug)]
 pub struct Config {
+    #[arg()]
     pub query: String,
+    #[arg()]
     pub filename: String,
+    #[arg(short, long)]
     pub case_sensitive: bool,
 }
 
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        // let query = &args[1];
-        // let filename = &args[2];
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
-        Ok(Config { query, filename, case_sensitive })
+pub fn parse_arguments() -> Config {
+    let mut cfg = Config::parse();
+    if env::var("CASE_INSENSITIVE").is_ok() {
+        cfg.case_sensitive = false;
     }
+    cfg
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
