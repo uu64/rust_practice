6269b07ce790a94779155665da30e168e8bdb6d8
uu64 <yusuke.t.0808@gmail.com>
2024-10-14 12:29:29 +0900

    refactor

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -14,17 +14,22 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let mut file = File::open("hello.html").unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
+    let mut file = File::open(filename).unwrap();
     let mut contents = String::new();
+
     file.read_to_string(&mut contents).unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
